4126b285b18e08510b50cda0b17dc61cc0ea4878
Volodymyr Miroshnycehnko <miroshveva98@gmail.com>
2022-01-27 13:21:17 +0200

    rearranged modules init seq

--- run.py
+++ run.py
@@ -3,23 +3,26 @@ import logging
 from src.app import create_app
 import src.utils as utils
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-# init api app
+# init API app
 auth_api = create_app()
 
 # init utils
+# JWT utils
 access_token_generator = utils.AccessTokenGenerator(auth_api.config)
 refresh_token_generator = utils.RefreshTokenGenerator(auth_api.config)
 jwt_decoder = utils.JWTDecoder(auth_api.config)
-reset_password_token_generator = utils.ResetPasswordTokenGenerator(auth_api.config)
-reset_password_token_decoder = utils.ResetPasswordTokenDecoder(auth_api.config)
-email_sender = utils.EmailSender(auth_api.config, auth_api.flask_app)
 refresh_token_storage_controller = utils.RefreshTokenStorageController(
     host=auth_api.config['R_HOST'],
     port=auth_api.config['R_PORT'],
     db=auth_api.config['R_JWT_DB'],
 )
+# reset PWD utils
+reset_password_token_generator = utils.ResetPasswordTokenGenerator(auth_api.config)
+reset_password_token_decoder = utils.ResetPasswordTokenDecoder(auth_api.config)
+# email utils
+email_sender = utils.EmailSender(auth_api.config, auth_api.flask_app)
 
 # get Flask app for actual run
 app = auth_api.flask_app
