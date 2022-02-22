4522bdcc437da3a714f973cd08845597821da6a3
Volodymyr Miroshnycehnko <miroshveva98@gmail.com>
2022-02-22 15:27:31 +0200

    refactored ABCs classes

--- src/abstractions/abc_token_decoder.py
+++ src/abstractions/abc_token_decoder.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class ABCTokenDecoder(ABC):
     @abstractmethod
     def decode_token(self, token: str) -> str:
-        pass
+        raise NotImplementedError
--- src/abstractions/abc_token_generator.py
+++ src/abstractions/abc_token_generator.py
@@ -5,4 +5,4 @@ from abc import ABC, abstractmethod
 class ABCTokenGenerator(ABC):
     @abstractmethod
     def create_token(self, claims: t.Dict[str, t.Any]) -> str:
-        pass
+        raise NotImplementedError
